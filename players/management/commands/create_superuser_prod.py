3fdb2814754f2c430ffc2032e2b07658abf82611
naoki-sato-ojiyan <feel.vip.nakky@gmail.com>
2026-06-12 12:21:15 +0900

    スーパーユーザー作成コマンドのパスワードを環境変数化

--- players/management/commands/create_superuser_prod.py
+++ players/management/commands/create_superuser_prod.py
@@ -1,12 +1,19 @@
 # players/management/commands/create_superuser_prod.py
 from django.core.management.base import BaseCommand
 from players.models import User
+import os
 
 class Command(BaseCommand):
     help = 'スーパーユーザーを作成する'
 
     def handle(self, *args, **options):
-        email = 'nakky19900709@gmail.com'
+        email = os.environ.get('SUPERUSER_EMAIL', 'nakky19900709@gmail.com')
+        password = os.environ.get('SUPERUSER_PASSWORD')
+
+        if not password:
+            self.stdout.write('SUPERUSER_PASSWORDが設定されていません。スキップします。')
+            return
+
         if User.objects.filter(email=email).exists():
             user = User.objects.get(email=email)
             user.is_staff = True
@@ -14,5 +21,5 @@ class Command(BaseCommand):
             user.save()
             self.stdout.write(f'既存ユーザーをスーパーユーザーに昇格: {email}')
         else:
-            User.objects.create_superuser(email=email, password='Nakky0804')
+            User.objects.create_superuser(email=email, password=password)
             self.stdout.write(f'スーパーユーザー作成完了: {email}')
